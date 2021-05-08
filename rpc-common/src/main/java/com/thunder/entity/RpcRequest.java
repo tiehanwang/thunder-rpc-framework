1a2396389f0adb98a7d1f90bf4c8bd9012974997
tiehanwang <50023951+tiehanwang@users.noreply.github.com>
2021-05-09 00:23:22 +0800

    注释相关添加

--- rpc-common/src/main/java/com/thunder/entity/RpcRequest.java
+++ rpc-common/src/main/java/com/thunder/entity/RpcRequest.java
@@ -11,11 +11,11 @@ import java.io.Serializable;
 @Data
 @Builder
 public class RpcRequest implements Serializable {
-    //函数名
+    //接口名
     private String interfaceName;
     //调用参数方法名
     private String methodName;
-    //待调用方法的参数
+    //待调用方法的参数值
     private Object[] parameters;
     //带调用方法参数的类型
     private Class<?>[] paramTypes;
